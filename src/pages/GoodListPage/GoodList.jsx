8fa8749d958ef751a3ffe85c64b8b46608d3ca29
EGRrqq <egrrqqdev@gmail.com>
2024-05-14 03:57:48 -0400

    feat: simple fetch data

--- src/pages/GoodListPage/GoodList.jsx
+++ src/pages/GoodListPage/GoodList.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+import { getProducts } from "../../services/api";
+
 function GoodList() {
-  return <h1>bruh</h1>;
+  const [products, setProducts] = useState(undefined);
+
+  useEffect(() => {
+    getProducts().then((data) => {
+      setProducts(data);
+    });
+  }, []);
+
+  return (
+    <article>
+      {products &&
+        products.map((i) => (
+          <section key={i.id}>
+            <h1>{i.name}</h1>
+          </section>
+        ))}
+    </article>
+  );
 }
 
 export default GoodList;

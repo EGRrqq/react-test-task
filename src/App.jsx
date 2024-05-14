99dfbd198e2df49060f0ccdcf83c9e357da28f32
EGRrqq <egrrqqdev@gmail.com>
2024-05-14 09:27:45 -0400

    feat: / route for goods list
    feat: NotFound page for not existing route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import GoodList from "./pages/GoodListPage/GoodList";
+import { Router, Route, Link, Switch } from "wouter";
+import NotFound from "./pages/NotFoundPage/NotFound";
 
 export default function App() {
   return (
-    <div>
-      <GoodList />
-    </div>
+    <Switch>
+      <Route path="/">
+        <main>
+          <GoodList />
+        </main>
+      </Route>
+
+      {/* Default Route (404) */}
+      <Route>{(params) => <NotFound params={params} />}</Route>
+    </Switch>
   );
 }

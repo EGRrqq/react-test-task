1416e5e44476a49d3bdc2eccac1f5f2721f4a154
EGRrqq <egrrqqdev@gmail.com>
2024-05-14 14:24:21 -0400

    feat: styles for selected aside image

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -3,10 +3,28 @@ import React, { useState } from "react";
 
 const ImageSlider = ({ images }) => {
   const [selectedImage, setSelectedImage] = useState(images[0]);
+  const prevImageRef = React.useRef();
 
-  function handleImageSelect(image) {
+  function handleMouseOver(target, image) {
+    handleSelectedStyles(target);
     setSelectedImage(image);
   }
+
+  function handleSelectedStyles(target) {
+    if (prevImageRef.current) {
+      prevImageRef.current.classList.add(
+        styles["image-slider__aside-img--not-selected"]
+      );
+      prevImageRef.current.classList.remove(
+        styles["image-slider__aside-img--selected"]
+      );
+    }
+
+    target.classList.remove(styles["image-slider__aside-img--not-selected"]);
+    target.classList.add(styles["image-slider__aside-img--selected"]);
+    prevImageRef.current = target;
+  }
+
   function handleNextClick() {
     const index = images.indexOf(selectedImage);
     setSelectedImage(images[(index + 1) % images.length]);
@@ -22,10 +40,11 @@ const ImageSlider = ({ images }) => {
         {images.map((image, index) => (
           <figure key={index}>
             <img
+              className={styles["image-slider__aside-img--not-selected"]}
               src={image}
               alt=""
               style={{ width: "50px", height: "50px" }}
-              onMouseOver={() => handleImageSelect(image)}
+              onMouseOver={(e) => handleMouseOver(e.target, image)}
             />
           </figure>
         ))}

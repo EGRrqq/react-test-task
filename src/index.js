af61561af964d1d735702c4f7961d7b2d3d1b963
EGRrqq <egrrqqdev@gmail.com>
2024-05-14 03:18:22 -0400

    feat: change deprecated ReactDOM.render method

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import App from "./App";
+import "./styles/index.css";
 
-import App from './App'
-
-import './styles/index.css'
-
-const rootView = document.getElementById('root')
-
-if (rootView) {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootView
-  )
-}
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
